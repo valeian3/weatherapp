22a42d2eddcc8876c6c7a8c608b6659e9d37156f
valerian <50135742+valeian3@users.noreply.github.com>
2024-09-12 18:11:33 +0200

    implement absolute path import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Search from "components/Search";
+
 function App() {
   return (
     <>
       <span className="text-slate-600">New app</span>
       <button className="btn-primary-filled">Primary Button</button>
+      <Search />
     </>
   );
 }
---
+++ src/components/Search.tsx
@@ -0,0 +1,12 @@
+function Search() {
+  return (
+    <input
+      type="search"
+      name="query"
+      placeholder="Zagreb.."
+      aria-label="Search city weather"
+    />
+  );
+}
+
+export default Search;
