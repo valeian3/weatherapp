236d042df4aadca682fd4b1636b0667bdff8dc93
valerian <50135742+valeian3@users.noreply.github.com>
2025-02-05 18:16:24 +0100

    security dependency update
    vite 5.4.8 -> 5.4.14, eslint 9.10.0 -> 9.19.0

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [react(), tsconfigPaths()],
+  css: {
+    postcss: "./postcss.config.js",
+  },
 });
